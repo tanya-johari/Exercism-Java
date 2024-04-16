d1c91e6af8ec8f321c5cd8d4805a0e250ddb13f2
tanyajohari <johtanya11@gmail.com>
2024-04-16 09:06:36 +0530

    Cook your Lasagna

---
+++ lasagna/src/main/java/Lasagna.java
@@ -0,0 +1,17 @@
+public class Lasagna {
+    public int expectedMinutesInOven(){
+        return 40;
+    }
+
+    public int remainingMinutesInOven(int actualTimeInOven){
+        return expectedMinutesInOven() - actualTimeInOven;
+    }
+
+    public int preparationTimeInMinutes(int layers){
+        return 2 * layers;
+    }
+
+    public int totalTimeInMinutes(int layers, int timeInOven){
+        return preparationTimeInMinutes(layers) + timeInOven;
+    }
+}
