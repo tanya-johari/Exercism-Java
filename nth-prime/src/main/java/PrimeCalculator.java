315d1322bfbc1b89625c5f0e1133054e71749cde
tanyajohari <johtanya11@gmail.com>
2024-04-28 22:04:35 +0530

    [Tanya] calculating nth prime using Sieve of Eratosthenes algorithm

--- nth-prime/src/main/java/PrimeCalculator.java
+++ nth-prime/src/main/java/PrimeCalculator.java
@@ -1,25 +1,34 @@
-class PrimeCalculator {
+import java.util.ArrayList;
+import java.util.Arrays;
 
+class Prime {
+    public static final int MAX_SIZE = 1000005;
+    static ArrayList<Integer> primes = new ArrayList<Integer>();
+
+    static void SieveOfEratosthenes() {
+        boolean[] isPrime = new boolean[MAX_SIZE];
+
+        Arrays.fill(isPrime, true);
+
+        for (int p = 2; p * p < MAX_SIZE; p++) {
+            if (isPrime[p]) {
+                for (int index = p * p; index < MAX_SIZE; index += p)
+                    isPrime[index] = false;
+            }
+        }
+
+        for (int p = 2; p < MAX_SIZE; p++)
+            if (isPrime[p])
+                primes.add(p);
+    }
+}
+class PrimeCalculator {
     int nth(int nth) {
         if (nth <= 0)
             throw new IllegalArgumentException();
 
-        int number = 2, count = 0;
-
-        while (count < nth) {
-            if (isPrime(number))
-                count++;
-
-            number++;
-        }
-        return number - 1;
-    }
-
-    boolean isPrime(int number) {
-        for (int i = 2; i <= Math.sqrt(number); i++) {
-            if (number % i == 0) return false;
-        }
-        return true;
+        Prime.SieveOfEratosthenes();
+        return Prime.primes.get(nth-1);
     }
 }
 
